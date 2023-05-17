79a246ec56ac51999c2c88b2e91dcfeea0fe312c
powerTan <454974779@qq.com>
2023-05-18 07:45:05 +0800

    Redis实现 如果一个外卖配送单子要发布，现在有200个骑手都想要接这一单，如何保证只有一个骑手接到单子？

--- src/main/java/com/tan/springboot2/p2/DeliveryController.java
+++ src/main/java/com/tan/springboot2/p2/DeliveryController.java
@@ -1,19 +1,26 @@
 package com.tan.springboot2.p2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @RestController
 @RequestMapping("/delivery")
 public class DeliveryController {
     private static final String DELIVERY_ORDER_ACCEPTED_KEY_PREFIX = "delivery_order_accepted:";
-    private static final long LOCK_EXPIRATION_SECONDS = 30;
+    private static final long LOCK_EXPIRATION_SECONDS = 300;
+    private static final long LOCK_RENEWAL_SECONDS = 10;
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
@@ -22,20 +29,40 @@ public class DeliveryController {
     public String publishDeliveryOrder(String orderId) {
         String key = DELIVERY_ORDER_ACCEPTED_KEY_PREFIX + orderId;
         ValueOperations<String, String> ops = redisTemplate.opsForValue();
-
-        // 尝试获取锁
-        boolean lockAcquired = ops.setIfAbsent(key, orderId, LOCK_EXPIRATION_SECONDS, TimeUnit.SECONDS);
-
-        if (lockAcquired) {
-            // 成功获取到锁，表示骑手接到了单子
-            // 这里可以保存配送单的详细信息到Redis中，使用Hash结构等
-
-            // 返回接单成功的消息
-            return "骑手已接到单子：" + orderId;
-        } else {
-            // 无法获取锁，表示已经有其他骑手接到了单子
-            // 返回接单失败的消息
-            return "该单子已被其他骑手接走";
+        String value = UUID.randomUUID().toString();
+        boolean lockAcquired=false;
+        try {
+            // 尝试获取锁
+             lockAcquired = ops.setIfAbsent(key, value, LOCK_EXPIRATION_SECONDS, TimeUnit.SECONDS);
+            if (lockAcquired) {
+                // 成功获取到锁，表示骑手接到了单子
+                // 这里可以保存配送单的详细信息到Redis中，使用Hash结构等
+                log.info("cg");
+                Thread.sleep(280*1000L);
+                // 返回接单成功的消息
+                return "骑手已接到单子：" + orderId;
+            } else {
+                log.info("sb");
+                // 返回接单失败的消息
+                return "该单子已被其他骑手接走" + orderId;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (lockAcquired) {
+                releaseLock(key, value);
+            }
         }
+        return key;
     }
+
+    private void releaseLock(String key, String value) {
+        RedisScript<Long> script = new DefaultRedisScript<>(
+                "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end",
+                Long.class
+        );
+        redisTemplate.execute(script, Collections.singletonList(key), value);
+    }
+
+
 }
