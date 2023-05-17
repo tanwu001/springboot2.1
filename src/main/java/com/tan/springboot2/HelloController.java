5358a48b536edcca605b4b650ed28691d1b4b1aa
powerTan <454974779@qq.com>
2023-05-17 11:39:47 +0800

    Initial commit

---
+++ src/main/java/com/tan/springboot2/HelloController.java
@@ -0,0 +1,14 @@
+package com.tan.springboot2;
+
+import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, world!";
+    }
+}
